fd0c93e4255db156a133d34a1a451c071b2f5ec2
Christian Nachtrieb <chris.nachtrieb@gmail.com>
2022-01-13 17:08:09 -0500

    Initial Commit

---
+++ Shared/SwiftfulMapAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftfulMapAppApp.swift
+//  Shared
+//
+//  Created by Christian Nachtrieb on 1/13/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftfulMapAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
